d368763b611165d058c30f7eb120d2602627c101
Julio Gazoli <julio_gazoli@hotmail.com>
2023-10-04 18:01:37 -0300

    asynchronous tasks
    - Celery
    - RabbitMQ

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import OrderItem
 from .forms import OrderCreateForm
 from cart.cart import Cart
+from .tasks import order_created
 
 
 def order_create(request):
@@ -13,8 +14,10 @@ def order_create(request):
             for item in cart:
                 OrderItem.objects.create(
                     order=order, product=item['product'], price=item['price'], quantity=item['quantity'])
-                # Clear the cart
+            # Clear the cart
             cart.clear()
+            # Launch asynchronous task
+            order_created.delay(order.id)
             return render(request, 'orders/order/created.html', {'order': order})
     else:
         form = OrderCreateForm()
