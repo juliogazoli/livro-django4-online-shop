9bab3cc6c81d6a1568f05bcd6fde7d653685fefa
Julio Gazoli <julio_gazoli@hotmail.com>
2023-11-20 17:52:19 -0300

    extending the administration site

--- myshop/orders/admin.py
+++ myshop/orders/admin.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 from django.contrib import admin
 from .models import Order, OrderItem
 from django.utils.safestring import mark_safe
+from django.urls import reverse
 
 
 def export_to_csv(modeladmin, request, queryset):
@@ -47,11 +48,17 @@ class OrderItemInline(admin.TabularInline):
     raw_id_fields = ['product']
 
 
+def order_detail(obj):
+    url = reverse('orders:admin_order_detail', args=[obj.id])
+    return mark_safe(f'<a href="{url}">View</a>')
+
+
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'first_name', 'last_name', 'email',
                     'address', 'postal_code', 'city', 'paid',
-                    order_payment, 'created', 'updated']
+                    order_payment, 'created', 'updated',
+                    order_detail]
     list_filter = ['paid', 'created', 'updated']
     inlines = [OrderItemInline]
     actions = [export_to_csv]
